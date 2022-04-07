d9272bd8d3973e87147e5e71252bf9a2833102cc
GedeDana <danasuyoga@gmail.com>
2022-04-07 16:59:58 +0800

    make some button form

--- buku.php
+++ buku.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/buku.css">
+    <script src="https://use.fontawesome.com/7d6592d6d3.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
@@ -19,28 +21,43 @@
         </nav>
     </header>
 
-    <div class="container d-flex justify-content-center mt-5">
-        <table class="table">
+    <div class="container flex-column justify-content-center mt-5 gap-3">
+        <button type="button" class="btn btn-danger mb-3">
+             <i class="fa fa-plus" aria-hidden="true"><span class="ps-2 pe-2">Tambah Data Buku</span></i>
+        </button>
+        <div class="tbl-container " style="overflow-x:auto;">
+        <table class="table table-striped rounded tbl-tag">
             <thead class="table-dark">
                 <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Judul Buku</th>
-                    <th scope="col">Pegarang</th>
-                    <th scope="col">Penerbit</th>
-                    <th scope="col">Jumlah Halaman</th>
-                    <th scope="col">Tahun Terbit</th>
-                    <th scope="col">Kategori</th>
+                    <th>Id</th>
+                    <th>Judul Buku</th>
+                    <th>Pegarang</th>
+                    <th>Penerbit</th>
+                    <th>Jumlah Halaman</th>
+                    <th>Tahun Terbit</th>
+                    <th>Kategori</th>
+                    <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <th scope="row">1</th>
                     <td>Mark</td>
-                    <td>Otto</td>
+                    <td>Otasdsadasdasdto</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
+                    <td>
+                        <div class="d-flex gap-3">
+                        <button type="button" class="btn btn-danger">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                        <button type="button" class="btn btn-warning">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
+                        </div>
+                    </td>
                 </tr>
                 <tr>
                     <th scope="row">2</th>
@@ -50,6 +67,16 @@
                     <td>@mdo</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
+                    <td>
+                        <div class="d-flex gap-3">
+                        <button type="button" class="btn btn-danger">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                        <button type="button" class="btn btn-warning">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
+                        </div>
+                    </td>
                 </tr>
                 <tr>
                     <th scope="row">3</th>
@@ -59,9 +86,20 @@
                     <td>@mdo</td>
                     <td>@mdo</td>
                     <td>@mdo</td>
+                    <td>
+                        <div class="d-flex gap-3">
+                        <button type="button" class="btn btn-danger">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                        <button type="button" class="btn btn-warning">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
+                        </div>
+                    </td>
                 </tr>
             </tbody>
         </table>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
