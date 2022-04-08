26c94b0468ce0bbe57eb45709c86df7d947389ab
GedeDana <danasuyoga@gmail.com>
2022-04-08 20:05:07 +0800

    add tambah_php

--- buku.php
+++ buku.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perpustakaan</title>
     <link rel="stylesheet" href="./css/buku.css">
     <script src="https://use.fontawesome.com/7d6592d6d3.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -23,8 +23,9 @@
 
     <div class="container flex-column justify-content-center mt-5 gap-3">
         <button type="button" class="btn btn-success mb-3">
-             <i class="fa fa-plus" aria-hidden="true"><span class="ps-2 pe-2">Tambah Data Buku</span></i>
+             <i class="fa fa-plus" aria-hidden="true"><span class="ps-2 pe-2"><a href="./tambah_buku.php" style="text-decoration: none !important; color: white;">Tambah Data Buku</a></span></i>
         </button>
+        
         <div class="tbl-container " style="overflow-x:auto;">
         <table class="table table-striped rounded tbl-tag">
             <thead class="table-dark">
---
+++ tambah_buku.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Perpustakaan</title>
+</head>
+<body>
+<header>
+        <nav class="navbar navbar-expand-lg bg-danger ">
+            <div class="container-fluid d-flex justify-content-center">
+                <p class="fw-normal fs-4 fw-light text-white">Data Buku Perpustakaan</p>
+            </div>
+        </nav>
+</header>
+<div class="container">
+<div class="d-flex justify-content-center align-items-center mt-5">
+        <div class="card">
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
