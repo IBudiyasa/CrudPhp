78c8ba1a07d406b3404c484221ad50cac3411f5a
GedeDana <danasuyoga@gmail.com>
2022-04-08 23:41:24 +0800

    make form input

--- tambah_buku.php
+++ tambah_buku.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,21 +8,54 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Perpustakaan</title>
 </head>
+
 <body>
-<header>
+    <header>
         <nav class="navbar navbar-expand-lg bg-danger ">
             <div class="container-fluid d-flex justify-content-center">
                 <p class="fw-normal fs-4 fw-light text-white">Data Buku Perpustakaan</p>
             </div>
         </nav>
-</header>
-<div class="container">
-<div class="d-flex justify-content-center align-items-center mt-5">
-        <div class="card">
-
-        </div>
-    </div>
-</div>
-
+    </header>
+    <div class="d-flex align-items-center light-blue-gradient" style="height: 100vh;">
+            <div class="container" >
+                <div class="d-flex justify-content-center">
+                    <div class="col-md-6">
+                        <div class="card rounded-0 shadow">
+                            <div class="card-body">
+                                <h3>Sign Up</h3>
+                                <form>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Full Name: </label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter full name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address:</label>
+                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Username: </label>
+                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password: </label>
+                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Confirm Password:</label>
+                                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 </body>
+
 </html>
